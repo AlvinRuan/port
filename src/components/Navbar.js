f50219761959f35846be31c80b6c982df3c9eeef
Alvin <alvin.rua@gmail.com>
2021-08-18 05:56:40 -0400

    Navbar completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,7 @@
 import React from "react";
 import logo from "../12345.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   return (
@@ -18,7 +20,7 @@ const Navbar = () => {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span className="navbar-toggler-icon"></span>
+            <FontAwesomeIcon icon={faBars} style={{ color: "#fff" }} />
           </button>
 
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
@@ -30,17 +32,17 @@ const Navbar = () => {
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="#">
-                  Link
+                  About Me
                 </a>
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="#">
-                  Link1
+                  Portfolio
                 </a>
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="#">
-                  Link2
+                  Contact
                 </a>
               </li>
             </ul>
